65cfd408cf0d7a2e3bc5dce26eff5d28510bd6eb
vladyslav <vladyslavzeleny@gmail.com>
2020-10-20 13:47:00 +0300

    The ability to update the profile has been implemented.

--- siteweather/forms.py
+++ siteweather/forms.py
@@ -1,12 +1,8 @@
 import re
+import requests
 
 from django import forms
-
 from django.contrib.auth import authenticate
-from django.contrib.auth.forms import UserCreationForm
-
-from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import get_object_or_404
 
 from siteweather import models
 
@@ -15,14 +11,22 @@ class CityBlockForm(forms.Form):
     city_name = forms.CharField(max_length=300, label='City name', widget=forms.TextInput(
         attrs={'class': 'form-control'}))
 
+    def clean(self):
+        data = self.cleaned_data
+        city_name = data.get('city_name')
+        try:
+            url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=21b7ad9e043e9a8fab161a49eafc3' \
+                  f'27f&units=metric'
+            r = requests.get(url).json()
+            city_weather = {
+                'weather_main_description': r['weather'][0]['main']
+            }
+        except KeyError:
+            self.add_error('city_name', f'City {city_name} was not found')
+        return data
 
-class UserRegisterForm(forms.Form):
-    username = forms.CharField(max_length=300, label='login', widget=forms.TextInput(
-        attrs={'class': 'form-control'}))
-    password = forms.CharField(max_length=300, label='password', widget=forms.PasswordInput(
-        attrs={'class': 'form-control'}))
-    password2 = forms.CharField(max_length=300, label='password2', widget=forms.PasswordInput(
-        attrs={'class': 'form-control'}))
+
+class BaseCustomUserForm(forms.Form):
     first_name = forms.CharField(max_length=300, label='first_name', widget=forms.TextInput(
         attrs={'class': 'form-control'}))
     last_name = forms.CharField(max_length=300, label='last_name', widget=forms.TextInput(
@@ -34,25 +38,14 @@ class UserRegisterForm(forms.Form):
 
     def clean(self):
         data = self.cleaned_data
-        if data.get('password') != data.get('password2'):
-            self.add_error('password2', 'The verification password does not match the entered one')
-        if len(data.get('username')) < 4:
-            self.add_error('username', 'Your username has to contain at least 4 symbols')
-        if ' ' in data.get('username'):
-            self.add_error('username', 'No spaces allowed')
         if len(data.get('first_name')) < 2:
-            self.add_error('first_name', 'First name error')
+            self.add_error('first_name', 'First name is too short')
         if len(data.get('last_name')) < 2:
-            self.add_error('last_name', 'Surname error')
-        check_username = models.CustomUser.objects.filter(username=data.get('username')).first()
-        if check_username:
-            self.add_error('username', 'Username is taken')
+            self.add_error('last_name', 'Surname is too short')
         if data.get('phone_number'):
             letters_check = (data.get('phone_number'))[1:].isdecimal()
             symbols_check = re.search(r'\W', data.get('phone_number')[1:])
             plus_check = re.search(r'\W', data.get('phone_number')[0])
-            print(plus_check)
-            print(data.get('phone_number')[0])
             if plus_check is not None and symbols_check is None and letters_check is True:
                 if data.get('phone_number')[0] != '+':
                     self.add_error('phone_number', "Only '+' is allowed at the beginning")
@@ -61,11 +54,35 @@ class UserRegisterForm(forms.Form):
         return data
 
 
-class UserLoginForm(forms.Form):
-    username = forms.CharField(max_length=300, label='username', widget=forms.TextInput(
+class UserRegisterForm(BaseCustomUserForm):
+    username = forms.CharField(max_length=300, label='login', widget=forms.TextInput(
         attrs={'class': 'form-control'}))
     password = forms.CharField(max_length=300, label='password', widget=forms.PasswordInput(
         attrs={'class': 'form-control'}))
+    password2 = forms.CharField(max_length=300, label='password2', widget=forms.PasswordInput(
+        attrs={'class': 'form-control'}))
+
+    def clean(self):
+        super(UserRegisterForm, self).clean()
+        data = self.cleaned_data
+        if data.get('password') != data.get('password2'):
+            self.add_error('password2', 'The verification password does not match the entered one')
+        if len(data.get('username')) < 4:
+            self.add_error('username', 'Your username has to contain at least 4 symbols')
+        if ' ' in str(data.get('username')):
+            self.add_error('username', 'No spaces allowed')
+        check_username = models.CustomUser.objects.filter(username=data.get('username')).first()
+        if check_username:
+            self.add_error('username', 'Username is taken')
+        return data
+
+
+class UserLoginForm(UserRegisterForm):
+    email = None
+    password2 = None
+    phone_number = None
+    first_name = None
+    last_name = None
 
     def clean(self):
         data = self.cleaned_data
@@ -77,7 +94,6 @@ class UserLoginForm(forms.Form):
         return data
 
 
-class UserUpdateForm(UserRegisterForm):
-    class Meta:
-        model = models.CustomUser
-        exclude = ('password', 'password2')
+class UserUpdateForm(BaseCustomUserForm):
+    photo = forms.ImageField(label='photo', widget=forms.FileInput(
+        attrs={'class': 'form-control'}), required=False,)
