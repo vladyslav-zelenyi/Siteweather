4dcfc59bd5a63072425ec128c3e39da027f85b13
vladyslav <vladyslavzelenyi@gmail.com>
2020-12-03 17:07:13 +0200

    Implemented API for registration.

--- siteweather/authentication/views.py
+++ siteweather/authentication/views.py
@@ -6,57 +6,77 @@ from django.contrib.auth.views import LoginView
 from django.core.mail import send_mail
 from django.shortcuts import redirect, render
 from django.views.generic.base import View
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
 
 from siteweather.authentication.forms import *
+from siteweather.serializers import RegistrationSerializer
 
 logger = logging.getLogger('django')
 
 
-class RegisterFormView(View):
-    form_class = UserRegisterForm
-    template_name = 'authentication/registration.html'
+class RegisterFormView(CreateAPIView):
+    serializer_class = RegistrationSerializer
 
-    def get(self, request, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            return redirect('siteweather:profile', pk=self.request.user.pk)
-        form = self.form_class(request.user)
-        return render(request, self.template_name, {'form': form})
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.user, request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            email = form.cleaned_data['email']
-            phone_number = form.cleaned_data['phone_number']
-            user_city = form.cleaned_data['city_name']
-            user = CustomUser.objects.create_user(
-                username=username,
-                password=password,
-                first_name=first_name,
-                last_name=last_name,
-                email=email,
-                phone_number=phone_number,
-                user_city=user_city,
-                role='Standard',
-            )
-            group = Group.objects.get(name='Registered')
-            user.groups.add(group)
-            permissions = group.permissions.all()
-            user.user_permissions.set(permissions)
-            login(request, user)
-            message = 'You have successfully registered on the site'
-            logger.info(f"{username} was registered and authorized")
-            send_mail(
-                subject='Registration',
-                from_email='Siteweather',
-                message=message,
-                recipient_list=[email]
-            )
-            return redirect('siteweather:profile:profile', pk=user.pk)
-        return render(request, self.template_name, {'form': form})
+    def perform_create(self, serializer):
+        user = CustomUser.objects.create_user(**serializer.data, role='Standard')
+        group = Group.objects.get(name='Registered')
+        user.groups.add(group)
+        permissions = group.permissions.all()
+        user.user_permissions.set(permissions)
+
+# class RegisterFormView(View):
+#     form_class = UserRegisterForm
+#     template_name = 'authentication/registration.html'
+#
+#     def get(self, request, *args, **kwargs):
+#         if self.request.user.is_authenticated:
+#             return redirect('siteweather:profile', pk=self.request.user.pk)
+#         form = self.form_class(request.user)
+#         return render(request, self.template_name, {'form': form})
+#
+#     def post(self, request, *args, **kwargs):
+#         form = self.form_class(request.user, request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             email = form.cleaned_data['email']
+#             phone_number = form.cleaned_data['phone_number']
+#             user_city = form.cleaned_data['city_name']
+#             user = CustomUser.objects.create_user(
+#                 username=username,
+#                 password=password,
+#                 first_name=first_name,
+#                 last_name=last_name,
+#                 email=email,
+#                 phone_number=phone_number,
+#                 user_city=user_city,
+#                 role='Standard',
+#             )
+#             group = Group.objects.get(name='Registered')
+#             user.groups.add(group)
+#             permissions = group.permissions.all()
+#             user.user_permissions.set(permissions)
+#             login(request, user)
+#             message = 'You have successfully registered on the site'
+#             logger.info(f"{username} was registered and authorized")
+#             send_mail(
+#                 subject='Registration',
+#                 from_email='Siteweather',
+#                 message=message,
+#                 recipient_list=[email]
+#             )
+#             return redirect('siteweather:profile:profile', pk=user.pk)
+#         return render(request, self.template_name, {'form': form})
 
 
 class UserLoginFormView(LoginView):
--- siteweather/serializers.py
+++ siteweather/serializers.py
@@ -1,6 +1,10 @@
+import re
+
+import requests
 from rest_framework import serializers
 
 from siteweather.models import CustomUser, CityBlock
+from task import settings
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -19,3 +23,71 @@ class CityBlockSerializer(serializers.ModelSerializer):
         fields = ['pk', 'city_name', 'weather_main_description', 'weather_full_description', 'timestamp',
                   'temperature', 'weather_icon', 'humidity', 'pressure', 'wind_speed', 'country',
                   'searched_by_user', 'customers']
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        max_length=150,
+        required=True,
+        help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'
+    )
+    password2 = serializers.CharField(max_length=300, write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'first_name', 'last_name', 'email', 'password', 'password2', 'phone_number', 'user_city']
+        extra_kwargs = {
+            'email': {'required': True},
+            'password2': {'required': True},
+            'user_city': {'required': True},
+        }
+
+    def validate_password2(self, value):
+        data = self.get_initial()
+        if data['password'] != value:
+            raise serializers.ValidationError('The verification password does not match the entered one')
+        return data
+
+    def validate_first_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError('First name is too short')
+        return value
+
+    def validate_last_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError('Surname is too short')
+        return value
+
+    def validate_phone_number(self, value):
+        if value:
+            letters_check = value[1:].isdecimal()
+            symbols_check = re.search(r'\W', value[1:])
+            plus_check = re.search(r'\W', value[0])
+            if plus_check is not None and symbols_check is None and letters_check is True:
+                if value[0] != '+':
+                    raise serializers.ValidationError('Only '+' is allowed at the beginning')
+            if letters_check is False or symbols_check is not None:
+                raise serializers.ValidationError('Only numbers are allowed')
+        return value
+
+    def validate_email(self, value):
+        if CustomUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError('User with entered email exists')
+        return value
+
+    def validate_user_city(self, value):
+        url = f'{settings.SITE_WEATHER_URL}?q={value}&appid={settings.APP_ID}&units=metric'
+        r = requests.get(url).json()
+        if r['cod'] == '404':
+            raise serializers.ValidationError('City was not found')
+        return value
+
+    def validate_username(self, value):
+        if len(value) < 4:
+            raise serializers.ValidationError('Your value has to contain at least 4 symbols')
+        if ' ' in str(value):
+            raise serializers.ValidationError('No spaces allowed')
+        check_username = CustomUser.objects.filter(username=value).exists()
+        if check_username:
+            raise serializers.ValidationError('Username is taken')
+        return value
